e81fc79275685e92954c2467427245e39b68ca66
diegojimenez77 <djpcoding8652@gmail.com>
2023-07-05 15:16:29 -0600

    adding operadores de comparacion

--- operadores.js
+++ operadores.js
@@ -61,3 +61,56 @@ let nickname = `Mi nombre es: ${nombre2} y mi apellido ${apellido2}`
 let nickname2 = `Mi nombre es: ${20>10} y mi apellido ${apellido2}`
 console.log(nickname)
 console.log(nickname2)
+
+/**
+ * 3.4 Operadores de comparacion
+ * 
+ */
+
+//Es igual ==
+
+let c = 50;
+let d = 10;
+
+let resultado = c == d
+console.log(resultado) // prints false
+
+let e = 50;
+let f = 50;
+
+let resultado2 = e == f
+console.log(resultado2) // prints true
+
+let g = 50;
+let h = "50";
+
+let resultado3 = g == h
+console.log(resultado3) // prints true, no esta comparando el tipo de dato solo el valor.
+
+let i = 50;
+let j = "50";
+
+let resultado4 = g === h
+console.log(resultado4) // prints false, es estrictamente igual?, compara valor y tipo de dato.
+
+// Es diferente, se usa el simbolo de negacion o exclamacion.
+
+let k = 50;
+let l = "50";
+
+let resultado5 = k != l
+console.log(resultado5) // prints false
+
+let m = 50;
+let n = "50";
+
+let resultado6 = m !== n
+console.log(resultado6) // prints true, es estrictamente diferente
+
+// podmos tambien comparar cadenas de texto
+
+let o = "Alberto"
+let p = "Alberto"
+
+let resultado7 = o !== p
+console.log(resultado7)
