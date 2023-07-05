e9510fc564447dbd4cd6fdc9c5b2cf11484fed04
diegojimenez77 <djpcoding8652@gmail.com>
2023-07-05 11:20:21 -0600

    first commit

---
+++ scritps.js
@@ -0,0 +1,22 @@
+// Esto es un comentario de una linea
+
+/*
+Este es un comentario de bloque....
+En este archivo se genera el listado de usuarios
+buscando por su ID en la base de datos
+o tambien buscando por medio de su correo electronico
+*/
+
+/** En este caso con el doble asterisco el 
+ * edtor automaticamente lo cierra 
+ * y va poniendo los asteriscos de cada linea
+ * es mas elegante y como de utilizar
+ */
+
+// En este caso todas las lineas se pueden comentar con ctrl+/ en windows y cmmd+shift+7 en mac
+// var hola = "Saludos";
+// console.log(hola);
+// console.log("Adios");
+
+console.log("Saludos desde VS Code");
+alert("Saludos desde alerta");
