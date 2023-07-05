55e254b0dce0cf96b008ace3ba8ce3402bbc5fe3
diegojimenez77 <djpcoding8652@gmail.com>
2023-07-05 14:50:24 -0600

    adding 3.2 operadores de asignacion

---
+++ operadores.js
@@ -0,0 +1,45 @@
+/**
+ * 3 - 3.1Operadores de asignacion
+ * el (=) es un operador de asignacin
+ */
+
+let alumno4 
+alumno4 = "Beto" // uso de operador de asignacion
+
+// Operadores aritmeticos basicos
+// son 5 (+, -, *, /, %)
+
+let operacion = 50 + 60;
+console.log(operacion);
+
+let operacion2 = 50 - 60;
+console.log(operacion2);
+
+let operacion3 = 5 * 60;
+console.log(operacion3);
+
+let operacion4 = 60 / 4;
+console.log(operacion4)
+
+let operacion5 = 60 % 4;
+console.log(operacion5);
+
+let numero1 = 10
+let numero2 = 4
+
+let operacion6 = numero1 + numero2;
+console.log(operacion6);
+
+/**
+ * 3.2 Operadores de asignacion
+ */
+let a = 10
+let b = 4
+
+a -= b //en un solo paso hace una suma y una asignacion esto quiere decir que suma b a a y reasigna el nuevo valor.
+// mismo se puede hacer con *, / y %, + y -
+
+// para exponencial se hace con ** console.log(a**b)
+// a **= b potencia con reasignacion.
+console.log(a ** b);
+
